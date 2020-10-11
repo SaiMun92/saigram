4b04f99a25a77e0f9bf4c0f92696d7613f520802
Lee Sai Mun <saimun92@gmail.com>
2020-10-11 10:25:00 +0800

    update README.md

--- src/comps/Title.tsx
+++ src/comps/Title.tsx
@@ -1,32 +1,29 @@
 import React, {useEffect, useState} from 'react';
-
+import axios from "axios";
 
 const Title = () => {
 
     const [quote, setQuote] = useState(null);
+    const fetch_quotes = async (): Promise<any> => {
+        try {
+            const response = await axios.get("https://type.fit/api/quotes");
+            const json_list = response.data;
+            const selector = Math.floor(Math.random() * json_list.length);
+            setQuote(json_list[selector]);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     useEffect(() => {
-        const fetch_quotes = async () => {
-            try {
-                const response = await fetch("https://type.fit/api/quotes");
-                const json_list = await response.json();
-                const selector = Math.floor(Math.random() * json_list.length) + 1;
-                setQuote(json_list[selector]);
-                if (!response.ok) {
-                    throw Error(response.statusText);
-                }
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetch_quotes();
+        fetch_quotes().then(r => r);
     }, []);
 
     return (
         <div className="title">
             <h1>SaiGram</h1>
             <h2>Your Pictures</h2>
-            <p>{quote && `${quote.text} - ${quote.author}`}</p>
+            <p data-testid="quote-test">{quote && `${quote.text} - ${quote.author}`}</p>
         </div>
     )
 }
