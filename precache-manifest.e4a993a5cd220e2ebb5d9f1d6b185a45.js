8b0932afc563c960296dd6001f09b34cffc6e5f8
sai mun <saimun92@gmail.com>
2020-10-11 10:22:43 +0800

    Updates

---
+++ precache-manifest.e4a993a5cd220e2ebb5d9f1d6b185a45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c837758bd083cb1c1a21c7ec7d1887ab",
+    "url": "/saigram/index.html"
+  },
+  {
+    "revision": "e8230a7978a518283bca",
+    "url": "/saigram/static/css/main.007327bc.chunk.css"
+  },
+  {
+    "revision": "50478371309d6a1bb178",
+    "url": "/saigram/static/js/2.50f684d0.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/saigram/static/js/2.50f684d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8230a7978a518283bca",
+    "url": "/saigram/static/js/main.bbb59370.chunk.js"
+  },
+  {
+    "revision": "675bc3f826765bc41bf0",
+    "url": "/saigram/static/js/runtime-main.dce04af5.js"
+  }
+]);
